ee05823556d4769046b5cf0e19f590a0f0832a42
Siavash Fallahdoust <siavashfdst@gmail.com>
2018-03-21 12:20:35 +0330

    Fix render text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render text: 'Hello World!'
+    render html: 'Hello World!'
   end
 end
